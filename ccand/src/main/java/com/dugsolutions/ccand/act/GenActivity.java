c8c07fa945fc7ef357fa68b08d31319793b589e4
Douglas Selph <douglasselph@gmail.com>
2017-07-03 17:39:35 -0500

    filling things out for CC

--- ccand/src/main/java/com/dugsolutions/ccand/act/GenActivity.java
+++ ccand/src/main/java/com/dugsolutions/ccand/act/GenActivity.java
@@ -1,20 +1,22 @@
-package com.dugsolutions.ccand;
+package com.dugsolutions.ccand.act;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.dugsolutions.ccand.R;
+
+public class GenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.gen_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
